8b2fc43f783e7ece81c5e1be43f402dc28230b96
Yehyus <yehyarefaat@hotmail.com>
2023-11-10 08:17:56 -0500

    Affichage des articles

--- js/futur.js
+++ js/futur.js
@@ -26,11 +26,56 @@
       data.forEach(function (article) {
         titre = article.title.rendered;
         console.log(titre);
+        afficherFutur();
+      
       });
     })
     .catch(function (error) {
       // Gérer les erreurs
       console.error("Erreur lors de la récupération des données :", error);
     });
-  
+
+    let divfutur1 = document.querySelector('.futur1');
+
+    function afficherFutur() {
+      let urlSession = "http://localhost:8080/5w5/wp-json/wp/v2/posts?categories=25&per_page=30";
+     
+      fetch(urlSession)
+        .then(response => response.json())
+        .then(data => {
+          // Clear the articleContainer
+          divfutur1.innerHTML = "";
+   
+          // Loop through the data and create article elements
+          data.forEach(article => {
+            const infoAte = document.createElement("div");
+            infoAte.className = "article";
+            infoAte.innerHTML = `
+            <div class='animation-rectangle'>
+              <div class='rectangle-deco1'></div>
+              <div class='rectangle-deco2'></div>
+              <div class='rectangle-deco3'></div>
+              <div class='rectangle-deco4'></div>
+              <div class='rectangle-deco5'></div>
+            </div>
+            <h2>${article.title.rendered}</h6>
+            <p>${article.content.rendered}</p>
+            <div class='animation-cube'>
+              <div class='cube1'>
+                  <div class='cube-deco1'></div>
+                  <div class='cube-deco2'></div>
+              </div>
+
+              <div class='cube2'>
+                  <div class='cube-deco3'></div>
+                  <div class='cube-deco4'></div>
+              </div>
+            </div>
+            `;
+            divfutur1.appendChild(infoAte);
+          });
+        })
+      .catch(error => console.log(error));
+    }
+
 })();
